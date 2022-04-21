8f91163e62e71feca31dd17a6f91b2e898014750
octave LE TULLIER <octave.letullier@telecom-paris.fr>
2022-04-21 20:03:56 +0200

    Add contacts page and rules page

--- src/views/aboutView.js
+++ src/views/aboutView.js
@@ -1,11 +1,58 @@
 import React from 'react'
 import "../styles/about.css"
 
+import OctaveImg from "../assets/octavePic.jpg";
+import {GitHubIcon, LinkedInIcon} from "../assets/Icons"
+
 export default function AboutView() {
   return (
     <div className='aboutContainer'>
-        About: Team contact/ project course?
+        About: Team contact/Project course?
+        <div>
+            {teamMemberData.map(data =>
+                <div className='content'>
+                    <p>{data.name}</p>
+                    <p>
+                        <a className="social" href={data.githubURL} target="_blank" rel="noreferrer" title="Open GitHub profile"><GitHubIcon /></a>
+                        <a className="social" href={data.linkedInURL} target="_blank" rel="noreferrer" title="Open LinkedIn profile"><LinkedInIcon /></a>
+                    </p>
+                    <img src={data.img} alt=""></img>
+                </div>)
+            }
+        </div>
     </div>
 
   )
 }
+
+const teamMemberData = [{
+        name: "Octave Le Tullier",
+        img: OctaveImg,
+        githubURL: "https://www.kth.se/",
+        linkedInURL: "https://www.kth.se/",
+    },
+    {
+        name: "Name",
+        img: "",
+        githubURL: "github",
+        linkedInURL: "linkedin",
+    },
+    {
+        name: "Name",
+        img: "",
+        githubURL: "github",
+        linkedInURL: "linkedin",
+    },
+    {
+        name: "Name",
+        img: "",
+        githubURL: "github",
+        linkedInURL: "linkedin",
+    },
+    {
+        name: "Name",
+        img: "",
+        githubURL: "github",
+        linkedInURL: "linkedin",
+    }
+]
--- src/views/rulesView.js
+++ src/views/rulesView.js
@@ -5,7 +5,60 @@ export default function RulesView() {
   return (
     <div className='rulesContainer'>
         Rules/Pitch?
+        <div className='content'>
+          {rulesData.map(rule => {
+            return(
+              <div className="card">
+                <div className="cardSide cardSide--front">
+                  <div className="cardDetails">
+                    <p className='title'>{rule.name}</p>
+                  </div>
+                </div>
+                <div className="cardSide cardSide--back">
+                  <div className="cardDetails">
+                    <p className='description'>{rule.description}</p>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>        
     </div>
 
   )
 }
+
+const rulesData = [
+  {
+    name: "Resources",
+    description: "Collect some resources and combine them to attack"
+  },
+  {
+    name: "Resources",
+    description: "Collect some resources and combine them to attack"
+  },
+  {
+    name: "Resources",
+    description: "Collect some resources and combine them to attack"
+  },
+  {
+    name: "Resources",
+    description: "Collect some resources and combine them to attack"
+  },
+  {
+    name: "Resources",
+    description: "Collect some resources and combine them to attack"
+  },
+  {
+    name: "Resources",
+    description: "Collect some resources and combine them to attack"
+  },
+  {
+    name: "Resources",
+    description: "Collect some resources and combine them to attack"
+  },
+  {
+    name: "Resources",
+    description: "Collect some resources and combine them to attack"
+  }
+]
